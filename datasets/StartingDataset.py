a76c8c579401fcb69b031ff625fcc8c9ff4a2cbb
Darren Zhang <darrenzhang22@gmail.com>
2021-02-18 19:48:40 -0800

    Efficient Net b4

--- datasets/StartingDataset.py
+++ datasets/StartingDataset.py
@@ -14,19 +14,25 @@ class StartingDataset(torch.utils.data.Dataset):
         label = self.truth[index]
 
         img = Image.open(f'{self.base}/{path}')
-        preprocess = transforms.Compose([
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-            transforms.Resize([224,224])
-        ])
+        # preprocess = transforms.Compose([
+        #     transforms.ToTensor(),
+        #     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        #     transforms.Resize([224,224])
+        # ])
         preprocess2 = transforms.Compose([
             transforms.Resize(256),
             transforms.CenterCrop(224),
             transforms.ToTensor(),
             transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
+        preprocess_for_efficient_net = transforms.Compose([
+            transforms.Resize(380),
+            transforms.CenterCrop(380),
+            transforms.ToTensor(),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        ])
 
-        tensor = preprocess2(img)
+        tensor = preprocess_for_efficient_net(img)
         return tensor, int(label)
 
     def __len__(self):
--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from efficientnet_pytorch import EfficientNet
 
 class StartingNetwork(torch.nn.Module):
     """
@@ -9,15 +10,21 @@ class StartingNetwork(torch.nn.Module):
 
     def __init__(self, input_dim, output_dim):
         super(StartingNetwork, self).__init__()
-        self.resnet = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True)
-        self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
+        # self.resnet = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True)
+        # self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
+
+        self.efficient_net = EfficientNet.from_pretrained('efficientnet-b7')
+        # print(EfficientNet.get_image_size('efficientnet-b4'))
+
+        # self.efficient_net = nn.Sequential(*list(self.efficient_net.children())[:-1])
         # self.conv1 = nn.Conv2d(input_dim, 64, kernel_size=3, stride=1, padding=1)
         # self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=1)
         # self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1)
         # # self.conv4 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
         # self.conv4 = nn.Conv2d(256, 10, kernel_size=3, stride=1)
         # self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.flatten_size = 512
+        self.flatten_size = 2560*12*12
+        # 512
         # 1000
         # 10 * 35 *48
         # 33600
@@ -25,36 +32,16 @@ class StartingNetwork(torch.nn.Module):
         self.fc1 = nn.Linear(self.flatten_size,128)
         self.fc2 = nn.Linear(128,output_dim)
         
-    def forward(self, x):
-        # x = torch.reshape(x,[-1,3,600,800])
-        # print('Beginning shape: ', x.shape)
-        # x = self.conv1(x)
-        # # print("finished conv1",x.shape)
-        # x = F.relu(x)
-        # # print("finished relu",x.shape)
-        # x = self.maxpool1(x)
-        # # print("finished maxpool",x.shape)
-        # x = self.conv2(x)
-        # x = F.relu(x)
-        # x = self.maxpool1(x)
-        # x = self.conv3(x)
-        # x = F.relu(x)
-        # x = self.maxpool1(x)
-        # x = self.conv4(x)
-        # # x = F.relu(x)
-        # # x = self.maxpool1(x)
-        # # x = self.conv5(x)
-        # x = F.relu(x)
-        # x = self.maxpool1(x)
-        # # print('Shape after CNN: ', x.shape)
+    def forward(self, x):   
         # with torch.no_grad():
-        x = self.resnet(x)
-        # print(x.shape)
+        # x = self.resnet(x)
+        # print("before "+str(x.shape))
+        with torch.no_grad():
+            x = self.efficient_net.extract_features(x)
+        # print(f'before squeeze {x.shape}')
         x = torch.reshape(x,[-1, self.flatten_size])
         # print('After reshaping',x.shape)
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
-        # print("SHape after FCN",x.shape)
-        # print(x)
         return x
