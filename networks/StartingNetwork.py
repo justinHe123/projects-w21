5b540abfa6764a683bbaa03d6864bd79c9a3191b
Darren Zhang <darrenzhang22@gmail.com>
2021-03-01 22:17:00 -0800

    Switch to resnet50

--- networks/StartingNetwork.py
+++ networks/StartingNetwork.py
@@ -10,8 +10,8 @@ class StartingNetwork(torch.nn.Module):
 
     def __init__(self, input_dim, output_dim):
         super(StartingNetwork, self).__init__()
-        self.resnet = torch.hub.load('pytorch/vision:v0.6.0', 'resnet18', pretrained=True)
-        self.conv1 = nn.Conv2d(input_dim, 3, kernel_size = 3, padding = 1)      
+        self.resnet = torch.hub.load('pytorch/vision:v0.6.0', 'resnet50', pretrained=True)
+        self.conv1 = nn.Conv2d(input_dim, 3, kernel_size = 3,padding=1)      
         self.resnet = nn.Sequential(*list(self.resnet.children())[:-1])
 
         # self.efficient_net = EfficientNet.from_pretrained('efficientnet-b7')
@@ -24,14 +24,16 @@ class StartingNetwork(torch.nn.Module):
         # # self.conv4 = nn.Conv2d(256, 512, kernel_size=3, stride=1, padding=1)
         # self.conv4 = nn.Conv2d(256, 10, kernel_size=3, stride=1)
         self.maxpool1 = nn.MaxPool2d(kernel_size=2, stride=2)
-        self.flatten_size = 512
+        self.flatten_size = 2048
+        # 512
         # 2560*12*12
         # 1000
         # 10 * 35 *48
         # 33600
         # 10*50*37
-        self.fc1 = nn.Linear(self.flatten_size,128)
-        self.fc2 = nn.Linear(128,output_dim)
+        self.fc1 = nn.Linear(self.flatten_size,1024)
+        self.fc2 = nn.Linear(1024,256)
+        self.fc3 = nn.Linear(256,output_dim)
         
     def forward(self, x):   
         x = self.conv1(x)
@@ -48,4 +50,6 @@ class StartingNetwork(torch.nn.Module):
         x = self.fc1(x)
         x = F.relu(x)
         x = self.fc2(x)
+        x = F.relu(x)
+        x = self.fc3(x)
         return x
